fdccfb0f5f9ac2aba34c3b03fb09f732b9c942e4
Igor <igor_1gdb@hotmail.com>
2018-06-14 09:37:44 -0300

    Create fragment class and layouts with item example Refs #9

---
+++ app/src/main/java/com/example/igor/projetopoo/fragment/ListFragment.java
@@ -0,0 +1,23 @@
+package com.example.igor.projetopoo.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.igor.projetopoo.R;
+
+public class ListFragment extends Fragment {
+
+
+    public ListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+
+}
