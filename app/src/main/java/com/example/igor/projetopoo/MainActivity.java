1e52ecd4f2c32da15e0c49da33f3a2dbadc8e1b9
Victor Pinheiro <vpinheiro.aguiar@gmail.com>
2018-06-18 16:25:59 -0300

    Added black background in search bar Refs #15

--- app/src/main/java/com/example/igor/projetopoo/MainActivity.java
+++ app/src/main/java/com/example/igor/projetopoo/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.igor.projetopoo;
 
+import android.graphics.drawable.TransitionDrawable;
+import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.mancj.materialsearchbar.MaterialSearchBar;
+import com.mancj.materialsearchbar.MaterialSearchBar.OnSearchActionListener;
+
+
+public class MainActivity extends AppCompatActivity implements OnSearchActionListener {
+
+    FrameLayout blackBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        blackBar = (FrameLayout) findViewById(R.id.blackBar);
+        MaterialSearchBar searchBar = (MaterialSearchBar) findViewById(R.id.searchBar);
+
+        searchBar.setOnSearchActionListener(this);
+    }
+
+    @Override
+    public void onSearchStateChanged(boolean enabled) {
+        TransitionDrawable background = (TransitionDrawable) blackBar.getBackground();
+
+        if (enabled) {
+            background.startTransition(300);
+        } else {
+            background.reverseTransition(300);
+        }
+    }
+
+    @Override
+    public void onSearchConfirmed(CharSequence text) {
+
+    }
+
+    @Override
+    public void onButtonClicked(int buttonCode) {
+
     }
 }
