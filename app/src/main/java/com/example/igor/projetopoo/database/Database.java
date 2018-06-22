4af2eacd56d3aeeec65c6c381c40d33207402938
Igor <igor_1gdb@hotmail.com>
2018-06-22 18:06:32 -0300

    Import firebase and create database class Refs #29

---
+++ app/src/main/java/com/example/igor/projetopoo/database/Database.java
@@ -0,0 +1,18 @@
+package com.example.igor.projetopoo.database;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+public class Database {
+    private static FirebaseFirestore firestore;
+
+    public static void initFirebase() {
+        Database.firestore = FirebaseFirestore.getInstance();
+    }
+}
