5f967d8b18b42bad006112c60220e4acef22afed
Igor <igor_1gdb@hotmail.com>
2018-06-23 11:46:00 -0300

    Add get functions Refs #32

--- app/src/main/java/com/example/igor/projetopoo/database/DatabaseRequests.java
+++ app/src/main/java/com/example/igor/projetopoo/database/DatabaseRequests.java
@@ -1,5 +1,13 @@
 package com.example.igor.projetopoo.database;
 
-public interface DatabaseRequests {
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
+public interface DatabaseRequests {
+    DocumentSnapshot getDocument(DocumentReference documentReference);
+    QuerySnapshot getDocuments(CollectionReference collectionReference);
+    QuerySnapshot getDocuments(Query query);
 }
