707833c408cd9d0acb16eeb475619307c32c110e
Igor <igor_1gdb@hotmail.com>
2018-06-25 22:24:05 -0300

    Add black background Refs #33

--- app/src/main/java/com/example/igor/projetopoo/activity/search/SearchActivity.java
+++ app/src/main/java/com/example/igor/projetopoo/activity/search/SearchActivity.java
@@ -1,37 +1,70 @@
 package com.example.igor.projetopoo.activity.search;
 
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.widget.FrameLayout;
 
 import com.example.igor.projetopoo.R;
 import com.example.igor.projetopoo.adapter.SuggestionAdapter;
 import com.example.igor.projetopoo.entities.Item;
 import com.mancj.materialsearchbar.MaterialSearchBar;
+import com.mancj.materialsearchbar.SimpleOnSearchActionListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
+    private MaterialSearchBar searchBar;
+    private FrameLayout blackBackground;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        MaterialSearchBar searchBar = findViewById(R.id.search_searchbar);
+        searchBar = findViewById(R.id.search_searchbar);
+        blackBackground = findViewById(R.id.black_search);
 
-        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-        SuggestionAdapter customSuggestionsAdapter = new SuggestionAdapter(inflater);
+        final SuggestionAdapter customSuggestionsAdapter = new SuggestionAdapter(getLayoutInflater());
         List<Item> suggestions = new ArrayList<>();
-
-        // TODO: Popular a lista de sugestões com os produtos vindos do banco de dados
-        /* for (int i = 0; i < 10; i++){
-            suggestions.add(new Product("Produto" + i, i *10.0 ));
-        }*/
 
         customSuggestionsAdapter.setSuggestions(suggestions);
         searchBar.setCustomSuggestionAdapter(customSuggestionsAdapter);
+
+        searchBar.addTextChangeListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                customSuggestionsAdapter.getFilter().filter(searchBar.getText());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+
+        });
+
+        searchBar.setOnSearchActionListener(new SimpleOnSearchActionListener() {
+            @Override
+            public void onSearchStateChanged(boolean enabled) {
+                TransitionDrawable background = (TransitionDrawable) blackBackground.getBackground();
+
+                if (enabled) {
+                    background.startTransition(300);
+                } else {
+                    background.reverseTransition(300);
+                }
+            }
+        });
 
     }
 
