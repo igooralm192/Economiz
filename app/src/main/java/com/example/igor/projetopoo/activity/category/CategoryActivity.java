dc9a9522c2eb581edbe3cfbf965264821d9da5a0
Igor <igor_1gdb@hotmail.com>
2018-07-02 18:54:11 -0300

    Add search adapter and implement search filter functions Refs #46

--- app/src/main/java/com/example/igor/projetopoo/activity/category/CategoryActivity.java
+++ app/src/main/java/com/example/igor/projetopoo/activity/category/CategoryActivity.java
@@ -1,20 +1,45 @@
 package com.example.igor.projetopoo.activity.category;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.example.igor.projetopoo.R;
+import com.example.igor.projetopoo.activity.search.SearchActivity;
+import com.example.igor.projetopoo.adapter.SuggestionAdapter;
+import com.example.igor.projetopoo.entities.Item;
 import com.example.igor.projetopoo.utils.Animation;
 import com.mancj.materialsearchbar.MaterialSearchBar;
 
-public class CategoryActivity extends AppCompatActivity implements MaterialSearchBar.OnSearchActionListener {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CategoryActivity extends AppCompatActivity implements
+        MaterialSearchBar.OnSearchActionListener,
+        SuggestionAdapter.OnItemViewClickListener {
     private Toolbar toolbar;
     private FrameLayout blackLayout;
     private MaterialSearchBar searchBar;
+    private List<Item> recentQueries;
+    private List<Item> recentQueriesClone;
+    private SharedPreferences sharedPreferences;
+    private static final String RECENT_QUERY = "Recent Queries";
+    public static final String RECENT_MESSAGE = "search.name.recent";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +54,75 @@ public class CategoryActivity extends AppCompatActivity implements MaterialSearc
 
         getSupportActionBar().setTitle("Categorias");
 
+        final SuggestionAdapter customSuggestionsAdapter = new SuggestionAdapter(getLayoutInflater());
+
+        /*List<Item> suggestions = new ArrayList<Item>();
+        suggestions.add(new Item(R.mipmap.ic_launcher_round, "Abacaxi", "recent"));
+        suggestions.add(new Item(R.mipmap.ic_launcher_round, "Banana", "recent"));
+        suggestions.add(new Item(R.mipmap.ic_launcher_round, "Carne", "product"));
+        suggestions.add(new Item(R.mipmap.ic_launcher_round, "Amendoim", "category"));*/
+
+        sharedPreferences = getSharedPreferences(RECENT_QUERY, 0);
+
+        recentQueries = loadRecentQueries();
+        recentQueriesClone = new ArrayList<>(recentQueries);
+
+        customSuggestionsAdapter.setOnItemViewClickListener(this);
+        customSuggestionsAdapter.setSuggestions(recentQueries);
+        searchBar.setCustomSuggestionAdapter(customSuggestionsAdapter);
+
+        searchBar.addTextChangeListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //customSuggestionsAdapter.getFilter().filter(searchBar.getText());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String s = editable.toString();
+                s = s.trim();
+                if (s.length() > 0) {
+                    List<Item> recent = new ArrayList<>();
+                    for (Item item: recentQueries) {
+                        if (recent.size() >= 2) break;
+                        if (item.getName().toLowerCase().startsWith( s.toLowerCase() ))
+                            recent.add(item);
+                    }
+
+                    // recent.addAll(getFilteredProducts());
+                    // recent.addAll(getFilteredCategories());
+
+                    searchBar.updateLastSuggestions(recent);
+                } else {
+                    if (searchBar.isSearchEnabled())
+                        searchBar.updateLastSuggestions(recentQueriesClone);
+
+                }
+            }
+
+        });
+
         searchBar.setOnSearchActionListener(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        List<Item> recent = new ArrayList<>();
+
+        for (Object object: searchBar.getLastSuggestions()) {
+            Item item = (Item) object;
+
+            if (item.getType().equals("recent")) {
+                recent.add(item);
+            }
+        }
+
+        saveRecentQueries(recent);
     }
 
     @Override
@@ -62,11 +155,102 @@ public class CategoryActivity extends AppCompatActivity implements MaterialSearc
 
     @Override
     public void onSearchConfirmed(CharSequence text) {
+        String newText = text.toString();
+        newText = newText.trim();
 
+        if (newText.length() != 0) {
+            Item item = new Item(R.drawable.ic_history_black_24dp, newText, "recent");
+
+            if (!recentQueriesClone.contains(item)) {
+                if (recentQueriesClone.size() == 2) recentQueriesClone.remove(1);
+
+                recentQueriesClone.add(0, item);
+                recentQueries.add(item);
+            }
+
+            searchBar.setLastSuggestions(recentQueriesClone);
+
+            this.saveRecentQueries(recentQueriesClone);
+
+            this.startActivity(newText, SearchActivity.class, RECENT_MESSAGE);
+
+            searchBar.disableSearch();
+        } else searchBar.showSuggestionsList();
     }
 
     @Override
     public void onButtonClicked(int buttonCode) {
 
+    }
+
+    @Override
+    public void onItemClick(View view) {
+        TextView query = view.findViewById(R.id.name_suggestion);
+
+        searchBar.setLastSuggestions(recentQueriesClone);
+        searchBar.disableSearch();
+
+        Map<String, Class> index = new HashMap<>();
+        index.put("recent", SearchActivity.class);
+        //index.put("product", );
+        index.put("category", CategoryActivity.class);
+
+        for (String type : index.keySet()) {
+            Item item = new Item(R.drawable.ic_history_black_24dp, query.getText().toString(), type);
+            int indItem = recentQueries.indexOf(item);
+
+            if (indItem != -1) {
+                this.startActivity(query.getText().toString(), index.get(type), RECENT_MESSAGE);
+            }
+        }
+    }
+
+    private void startActivity(String text, Class activity, String keyMessage) {
+        Intent intent = new Intent(this, activity);
+        intent.putExtra(keyMessage, text);
+        startActivity(intent);
+    }
+
+
+    private void saveRecentQueries(List<Item> recent) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        JSONArray array = new JSONArray();
+
+        for (Item item : recent) {
+            JSONObject object = item.toJson();
+            array.put(object.toString());
+        }
+
+        editor.putString("recent", array.toString());
+        editor.apply();
+    }
+
+    private List<Item> loadRecentQueries() {
+        List<Item> recent = new ArrayList<>();
+
+        try {
+            String arrayStr = sharedPreferences.getString("recent", null);
+
+            if (arrayStr != null) {
+                JSONArray array = new JSONArray(arrayStr);
+
+                for (int i = 0; i < array.length(); i++) {
+                    JSONObject object = new JSONObject((String) array.get(i));
+
+                    Item item = new Item(
+                            object.getInt("idIcon"),
+                            object.getString("name"),
+                            object.getString("type")
+                    );
+
+                    recent.add(item);
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return recent;
     }
 }
