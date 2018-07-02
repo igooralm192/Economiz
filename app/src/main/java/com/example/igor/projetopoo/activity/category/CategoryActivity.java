75b0110c6f021e52d5ba54aafa0ff2d78d0f651f
Igor <igor_1gdb@hotmail.com>
2018-07-02 18:33:47 -0300

    Add animation on click search icon Refs #46

--- app/src/main/java/com/example/igor/projetopoo/activity/category/CategoryActivity.java
+++ app/src/main/java/com/example/igor/projetopoo/activity/category/CategoryActivity.java
@@ -5,23 +5,31 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import com.example.igor.projetopoo.R;
+import com.example.igor.projetopoo.utils.Animation;
+import com.mancj.materialsearchbar.MaterialSearchBar;
 
-public class CategoryActivity extends AppCompatActivity {
+public class CategoryActivity extends AppCompatActivity implements MaterialSearchBar.OnSearchActionListener {
     private Toolbar toolbar;
+    private FrameLayout blackLayout;
+    private MaterialSearchBar searchBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
 
-
+        blackLayout = findViewById(R.id.black_category);
+        searchBar = findViewById(R.id.search_bar_category);
 
         toolbar = findViewById(R.id.toolbar_category);
         setSupportActionBar(toolbar);
 
-        toolbar.setTitle("Categorias");
+        getSupportActionBar().setTitle("Categorias");
+
+        searchBar.setOnSearchActionListener(this);
     }
 
     @Override
@@ -37,9 +45,28 @@ public class CategoryActivity extends AppCompatActivity {
 
         if (id == android.R.id.home) finish();
         else if (id == R.id.search_icon_category) {
-
+            Animation.openSearch(searchBar, getSupportActionBar(), blackLayout);
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSearchStateChanged(boolean enabled) {
+        if (enabled) {
+
+        } else {
+            Animation.closeSearch(searchBar, getSupportActionBar(), blackLayout);
+        }
+    }
+
+    @Override
+    public void onSearchConfirmed(CharSequence text) {
+
+    }
+
+    @Override
+    public void onButtonClicked(int buttonCode) {
+
     }
 }
