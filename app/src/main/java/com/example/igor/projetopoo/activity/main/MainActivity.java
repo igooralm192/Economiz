da681a75df006d89e399b43aa5cb474c210bc54f
Igor de Almeida <igor_1gdb@hotmail.com>
2018-07-01 22:24:56 -0300

    Main fragment (#48)
    * Create fragment class and layouts with item example
    Refs #9
    
    * Create instance function and interaction list adjusts
    Refs #10
    
    * Create list generic adapter and your interface
    Refs #12
    
    * Improve fragment layout
    Refs #30

--- app/src/main/java/com/example/igor/projetopoo/activity/main/MainActivity.java
+++ app/src/main/java/com/example/igor/projetopoo/activity/main/MainActivity.java
@@ -4,13 +4,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.drawable.TransitionDrawable;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -18,8 +24,13 @@ import android.widget.TextView;
 
 import com.example.igor.projetopoo.R;
 import com.example.igor.projetopoo.activity.search.SearchActivity;
+import com.example.igor.projetopoo.adapter.ListAdapter;
+import com.example.igor.projetopoo.adapter.ListGenericAdapter;
 import com.example.igor.projetopoo.adapter.SuggestionAdapter;
+import com.example.igor.projetopoo.entities.Category;
 import com.example.igor.projetopoo.entities.Item;
+import com.example.igor.projetopoo.fragment.ListFragment;
+import com.example.igor.projetopoo.helper.Blur;
 import com.mancj.materialsearchbar.MaterialSearchBar;
 import com.mancj.materialsearchbar.MaterialSearchBar.OnSearchActionListener;
 
@@ -53,6 +64,10 @@ public class MainActivity extends AppCompatActivity implements OnSearchActionLis
         setContentView(R.layout.activity_main);
 
         blackBar = (FrameLayout) findViewById(R.id.blackBar);
+
+        searchBar.setOnSearchActionListener(this);
+
+        setCategoryList(this);
 
         makeSearchBar();
     }
@@ -201,6 +216,53 @@ public class MainActivity extends AppCompatActivity implements OnSearchActionLis
         startActivity(intent);
     }
 
+    private void setCategoryList(final Context context) {
+        ArrayList<Category> categories = new ArrayList<Category>();
+        categories.add(new Category("Alimentos", R.drawable.food));
+        categories.add(new Category("Limpeza", R.drawable.cleaning2));
+        categories.add(new Category("Farmácia", R.drawable.farmacia));
+        categories.add(new Category("Zoológico", R.drawable.food));
+
+        final RecyclerView.LayoutManager layout = new LinearLayoutManager(this,
+                LinearLayoutManager.VERTICAL, false);
+
+        final ListGenericAdapter<Category, Category.Holder> listGenericAdapter = new ListGenericAdapter<Category, Category.Holder>(this, categories, new ListAdapter<Category, Category.Holder>() {
+            @Override
+            public Category.Holder onCreateViewHolder(Context context, @NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(context).inflate(R.layout.category_view, parent, false);
+
+                Category.Holder holder = new Category.Holder(view);
+
+                return holder;
+            }
+
+            @Override
+            public void onBindViewHolder(List<Category> items, @NonNull Category.Holder holder, int position) {
+                Category category = items.get(position);
+                holder.nameCategory.setText(category.getName());
+                holder.backCategory.setImageResource(category.getBackground());
+
+                Blur.blurImage(holder.backCategory, 5, context);
+            }
+        });
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        // Exemplo de instanciação do ListFragment
+        ListFragment listFragment = ListFragment.getInstance(new ListFragment.OnListFragmentSettings() {
+            @Override
+            public RecyclerView setList(RecyclerView lista) {
+                lista.setAdapter(listGenericAdapter);
+                lista.setLayoutManager(layout);
+
+                return lista;
+            }
+        });
+
+        fragmentTransaction.add(R.id.constraint_layout_main, listFragment);
+        fragmentTransaction.commit();
+    }
 
     private void saveRecentQueries(List<Item> recent) {
         SharedPreferences.Editor editor = sharedPreferences.edit();
---
+++ app/src/main/java/com/example/igor/projetopoo/helper/Blur.java
@@ -0,0 +1,68 @@
+package com.example.igor.projetopoo.helper;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
+import android.widget.ImageView;
+
+public class Blur {
+
+    private static Context context;
+
+    public static void blurImage(ImageView imageView, int radius, Context cont) {
+        context = cont;
+
+        BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+        Bitmap blurred = blurRenderScript(bitmap, radius); //second parametre is radius
+        imageView.setImageBitmap(blurred);
+    }
+
+    public static Bitmap blurRenderScript(Bitmap smallBitmap, int radius) {
+        try {
+            smallBitmap = RGB565toARGB888(smallBitmap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(
+                smallBitmap.getWidth(), smallBitmap.getHeight(),
+                Bitmap.Config.ARGB_8888);
+
+        RenderScript renderScript = RenderScript.create(context);
+
+        Allocation blurInput = Allocation.createFromBitmap(renderScript, smallBitmap);
+        Allocation blurOutput = Allocation.createFromBitmap(renderScript, bitmap);
+
+        ScriptIntrinsicBlur blur = ScriptIntrinsicBlur.create(renderScript,
+                Element.U8_4(renderScript));
+        blur.setInput(blurInput);
+        blur.setRadius(radius); // radius must be 0 < r <= 25
+        blur.forEach(blurOutput);
+
+        blurOutput.copyTo(bitmap);
+        renderScript.destroy();
+
+        return bitmap;
+
+    }
+
+    private static Bitmap RGB565toARGB888(Bitmap img) throws Exception {
+        int numPixels = img.getWidth() * img.getHeight();
+        int[] pixels = new int[numPixels];
+
+        //Get JPEG pixels.  Each int is the color values for one pixel.
+        img.getPixels(pixels, 0, img.getWidth(), 0, 0, img.getWidth(), img.getHeight());
+
+        //Create a Bitmap of the appropriate format.
+        Bitmap result = Bitmap.createBitmap(img.getWidth(), img.getHeight(), Bitmap.Config.ARGB_8888);
+
+        //Set RGB pixels.
+        result.setPixels(pixels, 0, result.getWidth(), 0, 0, result.getWidth(), result.getHeight());
+        return result;
+    }
+}
