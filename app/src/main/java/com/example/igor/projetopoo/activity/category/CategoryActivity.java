0de9872f1fa9562ab06d762d1ffb399946c1ab33
Igor <igor_1gdb@hotmail.com>
2018-07-02 13:37:43 -0300

    Initial commit Refs #37

---
+++ app/src/main/java/com/example/igor/projetopoo/activity/category/CategoryActivity.java
@@ -0,0 +1,15 @@
+package com.example.igor.projetopoo.activity.category;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.igor.projetopoo.R;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+    }
+}
