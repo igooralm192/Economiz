7c73421e7c40f9d8a6b6339e5e4b6f9ab5fc6c2e
Igor de Almeida <igor_1gdb@hotmail.com>
2018-07-01 22:48:55 -0300

    Search settings (#40)
    * Set settings of search bar
    Refs #33
    
    * Add filter
    Refs #33
    
    * Add black background
    Refs #33
    
    * Update layouts and adapter, add query functions
    Refs #33
    
    * Update activity functions
    Refs #33

--- app/src/main/java/com/example/igor/projetopoo/activity/search/SearchActivity.java
+++ app/src/main/java/com/example/igor/projetopoo/activity/search/SearchActivity.java
@@ -1,50 +1,241 @@
 package com.example.igor.projetopoo.activity.search;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.igor.projetopoo.R;
-import com.example.igor.projetopoo.activity.adapter.SuggestionAdapter;
+import com.example.igor.projetopoo.activity.main.MainActivity;
+import com.example.igor.projetopoo.adapter.SuggestionAdapter;
+import com.example.igor.projetopoo.entities.Item;
 import com.mancj.materialsearchbar.MaterialSearchBar;
+import com.mancj.materialsearchbar.SimpleOnSearchActionListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import static com.example.igor.projetopoo.R.id.toolbar;
+public class SearchActivity extends AppCompatActivity implements
+        MaterialSearchBar.OnSearchActionListener,
+        SuggestionAdapter.OnItemViewClickListener {
 
-public class SearchActivity extends AppCompatActivity {
+    private MaterialSearchBar searchBar;
+    private FrameLayout blackBackground;
+    private List<Item> recentQueries;
+    private List<Item> recentQueriesClone;
+    private SharedPreferences sharedPreferences;
+    private static final String RECENT_QUERY = "Recent Queries";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        MaterialSearchBar searchBar = findViewById(toolbar);
-        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
 
-        SuggestionAdapter customSuggestionsAdapter = new SuggestionAdapter(inflater);
-        List<Item> suggestions = new ArrayList<>();
+        searchBar = findViewById(R.id.search_searchbar);
+        blackBackground = findViewById(R.id.black_search);
+        sharedPreferences = getSharedPreferences(RECENT_QUERY, 0);
 
-        // TODO: Popular a lista de sugestões com os produtos vindos do banco de dados
-        /* for (int i = 0; i < 10; i++){
-            suggestions.add(new Product("Produto" + i, i *10.0 ));
-        }*/
+        //Intent intent = getIntent();
+        //String query = intent.getStringExtra(MainActivity.RECENT_MESSAGE);
+        //searchBar.setPlaceHolder(query);
 
-        customSuggestionsAdapter.setSuggestions(suggestions);
-        searchBar.setCustomSuggestionAdapter(customSuggestionsAdapter);
-
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        blackBackground.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if (searchBar.isSearchEnabled())
+                    searchBar.disableSearch();
             }
         });
+
+        recentQueries = loadRecentQueries();
+        recentQueriesClone = new ArrayList<>(recentQueries);
+
+        final SuggestionAdapter customSuggestionsAdapter = new SuggestionAdapter(getLayoutInflater());
+        customSuggestionsAdapter.setOnItemViewClickListener(this);
+        customSuggestionsAdapter.setSuggestions(recentQueries);
+        searchBar.setCustomSuggestionAdapter(customSuggestionsAdapter);
+
+        searchBar.addTextChangeListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String s = editable.toString();
+
+                if (s.length() > 0) {
+                    List<Item> recent = new ArrayList<>();
+                    for (Item item: recentQueries) {
+                        if (recent.size() >= 2) break;
+                        if (item.getName().toLowerCase().startsWith( s.toLowerCase() ))
+                            recent.add(item);
+                    }
+
+                    // recent.addAll(getFilteredProducts());
+                    // recent.addAll(getFilteredCategories());
+
+                    searchBar.updateLastSuggestions(recent);
+                } else {
+                    if (searchBar.isSearchEnabled())
+                        searchBar.updateLastSuggestions(recentQueriesClone);
+
+                }
+
+            }
+
+        });
+
+        searchBar.setOnSearchActionListener(this);
+
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        List<Item> recent = new ArrayList<>();
+
+        for (Object object: searchBar.getLastSuggestions()) {
+            Item item = (Item) object;
+
+            if (item.getType().equals("recent")) {
+                recent.add(item);
+            }
+        }
+
+        saveRecentQueries(recent);
+    }
+
+    private void saveRecentQueries(List<Item> recent) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        JSONArray array = new JSONArray();
+
+        for (Item item : recent) {
+            JSONObject object = item.toJson();
+            array.put(object.toString());
+        }
+        Log.i("TAG", array.toString());
+        editor.putString("recent", array.toString());
+        editor.apply();
+    }
+
+    private List<Item> loadRecentQueries() {
+        List<Item> recent = new ArrayList<>();
+
+        try {
+            String arrayStr = sharedPreferences.getString("recent", null);
+
+            if (arrayStr != null) {
+                JSONArray array = new JSONArray(arrayStr);
+
+                for (int i=0; i<array.length(); i++) {
+                    JSONObject object = new JSONObject((String) array.get(i));
+
+                    Item item = new Item(
+                            object.getInt("idIcon"),
+                            object.getString("name"),
+                            object.getString("type")
+                    );
+
+                    recent.add(item);
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return recent;
+    }
+
+    @Override
+    public void onSearchStateChanged(boolean enabled) {
+        TransitionDrawable background = (TransitionDrawable) blackBackground.getBackground();
+
+        if (enabled) background.startTransition(300);
+        else background.reverseTransition(300);
+    }
+
+    @Override
+    public void onSearchConfirmed(CharSequence text) {
+        String newText = text.toString();
+        newText = newText.trim();
+        searchBar.setPlaceHolder(newText);
+
+        Item item = new Item(R.drawable.ic_history_black_24dp, newText, "recent");
+        if (newText.length() != 0)
+            if (!recentQueriesClone.contains(item)) {
+                if (recentQueriesClone.size() == 2) recentQueriesClone.remove(1);
+
+                recentQueriesClone.add(0, item);
+                recentQueries.add(item);
+            }
+
+
+        searchBar.setLastSuggestions(recentQueriesClone);
+        searchBar.disableSearch();
+    }
+
+    @Override
+    public void onButtonClicked(int buttonCode) {
+
+
+    }
+
+    @Override
+    public void onItemClick(View view) {
+        TextView query = view.findViewById(R.id.name_suggestion);
+
+        searchBar.disableSearch();
+
+        Map<String, Class> index = new HashMap<>();
+        index.put("recent", SearchActivity.class);
+        //index.put("product", );
+        //index.put("category", );
+
+        for (String type : index.keySet()) {
+            Item item = new Item(R.drawable.ic_history_black_24dp, query.getText().toString(), type);
+            int indItem = recentQueries.indexOf(item);
+
+            if (indItem != -1) {
+                if (type.equals("recent")) {
+                    searchBar.setPlaceHolder(item.getName());
+                } else {
+                    //this.startActivity(query.getText().toString(), index.get(type), MainActivity.RECENT_MESSAGE);
+                }
+            }
+        }
+    }
+
+    private void startActivity(String text, Class activity, String keyMessage) {
+        Intent intent = new Intent(this, activity);
+        intent.putExtra(keyMessage, text);
+        startActivity(intent);
+    }
 }
