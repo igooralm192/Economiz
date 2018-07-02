4d76dfc72b1f75437f82f63a440a2968e35f847c
Igor <igor_1gdb@hotmail.com>
2018-07-02 17:46:27 -0300

    Add toolbar, back button and search icon Refs #45

--- app/src/main/java/com/example/igor/projetopoo/activity/category/CategoryActivity.java
+++ app/src/main/java/com/example/igor/projetopoo/activity/category/CategoryActivity.java
@@ -2,14 +2,44 @@ package com.example.igor.projetopoo.activity.category;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.igor.projetopoo.R;
 
 public class CategoryActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+
+
+
+        toolbar = findViewById(R.id.toolbar_category);
+        setSupportActionBar(toolbar);
+
+        toolbar.setTitle("Categorias");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar_category, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) finish();
+        else if (id == R.id.search_icon_category) {
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
