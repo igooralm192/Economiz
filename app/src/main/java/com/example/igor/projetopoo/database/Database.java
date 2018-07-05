c1179f78e25add7f0ec3b6ccd8043c75a2d1e276
Luis Grappi <luisfg99@hotmail.com>
2018-07-05 14:58:51 -0300

    Pull database Refs #67

---
+++ app/src/main/java/com/example/igor/projetopoo/database/Database.java
@@ -0,0 +1,125 @@
+package com.example.igor.projetopoo.database;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.util.Pair;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Database implements DatabaseRequests {
+    private Query query;
+    private QuerySnapshot querySnapshot;
+    private FirebaseFirestore firestore;
+    private DocumentSnapshot documentSnapshot;
+    private DocumentReference documentReference;
+
+    public Database(FirebaseFirestore firestore) { this.firestore = firestore; }
+    
+    public FirebaseFirestore getFirestore() { return this.firestore; }
+
+    public DocumentSnapshot getDocument(DocumentReference documentReference) {
+        documentReference.get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        Database.this.documentSnapshot = documentSnapshot;
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+        return documentSnapshot;
+    }
+
+    public QuerySnapshot getDocuments(CollectionReference collectionReference) {
+        collectionReference.get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        querySnapshot = queryDocumentSnapshots;
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+
+        return querySnapshot;
+    }
+
+    public QuerySnapshot getDocuments(Query query) {
+        query.get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        querySnapshot = queryDocumentSnapshots;
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+
+        return querySnapshot;
+    }
+
+    public DocumentReference addDocument(CollectionReference collectionReference, Object object) {
+        collectionReference.add(object)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Database.this.documentReference = documentReference;
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+
+        return documentReference;
+    }
+
+    public void updateDocument(DocumentReference documentReference, Object object) {
+        documentReference.set(object).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+    }
+
+    public void deleteDocument(DocumentReference documentReference) {
+        documentReference.delete().addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+    }
+
+}
