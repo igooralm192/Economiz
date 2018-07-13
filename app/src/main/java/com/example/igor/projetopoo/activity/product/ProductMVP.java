fce522ca813fadeb3a2876f76fb5d4556d7c97ff
Christian <christian.elster0@gmail.com>
2018-07-13 19:54:06 -0300

    Remove main functions Refs #89

--- app/src/main/java/com/example/igor/projetopoo/activity/product/ProductMVP.java
+++ app/src/main/java/com/example/igor/projetopoo/activity/product/ProductMVP.java
@@ -10,26 +10,21 @@ import java.util.List;
 public class ProductMVP {
     public interface PresenterOps {
         // Presenter methods => View acess
-        void getCategoryList();
-        void getAllSuggestions(Activity activity);
+
     }
 
     public interface ModelOps {
         // Model methods => Presenter acess
-        void categoryListRequest();
-        void suggestionsRequest();
+
     }
 
     public interface ReqPresenterOps {
         // Presenter methods => Model acess
-        void onReturnedCategoryList(List<Object> objects);
-        void onReturnedAllSuggestions(List<Object> objects);
+
     }
 
     public interface ReqViewOps {
         //View methods => Presenter access
-        void showCategories(List<Category> categories);
-        void showProgressBar(Boolean enabled);
-        void saveAllSuggestions(List<Category> categories, List<Product> products);
+
     }
 }
