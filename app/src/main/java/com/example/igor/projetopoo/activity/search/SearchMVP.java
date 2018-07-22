3d8612329fbfafa332e6bc90ad6d6b518c8e4982
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2018-07-22 12:52:05 -0300

    Add documentation in search package

--- app/src/main/java/com/example/igor/projetopoo/activity/search/SearchMVP.java
+++ app/src/main/java/com/example/igor/projetopoo/activity/search/SearchMVP.java
@@ -7,34 +7,32 @@ import com.example.igor.projetopoo.exception.DatabaseException;
 
 import java.util.List;
 
+/**
+ * Interface utilizada para definir as outras interfaces necessárias ao padrão MVP (Model-View-Presenter)
+ * referente a tela de pesquisa
+ */
 public interface SearchMVP {
 
+    // Métodos implementados pela Presenter (SearchPresenter) e os quais a View (SearchActivity) tem acesso
     interface PresenterOps{
-        // Presenter methods => View acess
         void getResultList(String query);
     }
 
+    // Métodos implementados pela Model (SearchModel) e os quais a Presenter tem acesso
     interface ModelOps{
-        // Model methods => Presenter acess
         void resultListRequest(String query, String upperbound) throws ConnectionException, DatabaseException;
     }
 
+     // Métodos implementados pela Presenter e os quais o Model tem acesso
     interface ReqPresenterOps{
-        // Presenter methods => Model acess
         void onReturnedResultList(List<Object> objects);
     }
 
+
+     // Métodos implementados pela View e os quais a Presenter tem accesso
     interface ReqViewOps{
-        // View methods => Presenter acess
         void showResults(List<Category> categoryList, List<Product> productList);
         void showProgressBar(Boolean enabled);
     }
-
-
-
-
-
-
-
 
 }
